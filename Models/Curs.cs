4c6f98d02588eade5e109d2a240bed70d4c84e57
alexandrazaharie <alexandra.zaharie@stud.ubbcluj.ro>
2024-02-17 16:37:21 +0200

    Prima modificare

---
+++ Models/Curs.cs
@@ -0,0 +1,10 @@
+﻿namespace Zaharie_Alexandra_Proiect.Models
+{
+    public class Curs
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Credits { get; set; }
+    }
+}
